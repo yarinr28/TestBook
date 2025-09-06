31da622e16a4c5ed1a31a0a77e44ef0633e7b1b8
unknown <aviacohen035@gmail.com>
2025-09-06 13:25:39 +0300

    added gql module to app

--- apps/nest-test/src/app/app.module.ts
+++ apps/nest-test/src/app/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { YogaDriver, YogaDriverConfig } from '@graphql-yoga/nestjs'
+import { GraphQLModule } from '@nestjs/graphql';
 
 @Module({
-  imports: [],
+  imports: [  GraphQLModule.forRoot<YogaDriverConfig>({
+      driver: YogaDriver
+    })],
   controllers: [AppController],
   providers: [AppService],
 })
